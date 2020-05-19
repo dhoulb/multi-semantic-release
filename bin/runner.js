2ec74659b41fbfc950e05e6c7610e7724babd0b1
Anton Golub <mailbox@antongolub.ru>
2020-05-19 22:21:08 +0300

    chore: minor improvements

--- bin/runner.js
+++ bin/runner.js
@@ -1,13 +1,22 @@
 module.exports = (flags) => {
+	// Capture exit control.
+	const exit = process.exit;
+	process.exit = () => {};
+
 	// Imports.
 	const getWorkspacesYarn = require("../lib/getWorkspacesYarn");
 	const multiSemanticRelease = require("../lib/multiSemanticRelease");
+	const multisemrelPkgJson = require("../package.json");
+	const semrelPkgJson = require("semantic-release/package.json");
 
 	// Get directory.
 	const cwd = process.cwd();
 
 	// Catch errors.
 	try {
+		console.log(`multi-semantic-release version: ${multisemrelPkgJson.version}`);
+		console.log(`semantic-release version: ${semrelPkgJson.version}`);
+
 		// Get list of package.json paths according to Yarn workspaces.
 		const paths = getWorkspacesYarn(cwd);
 		console.log("yarn paths", paths);
@@ -16,17 +25,18 @@ module.exports = (flags) => {
 		multiSemanticRelease(paths, {}, { cwd }).then(
 			() => {
 				// Success.
-				process.exit(0);
+				console.log(`[multi-semantic-release]:`, `process.exit(0)`);
+				exit(0);
 			},
 			(error) => {
 				// Log out errors.
 				console.error(`[multi-semantic-release]:`, error);
-				process.exit(1);
+				exit(1);
 			}
 		);
 	} catch (error) {
 		// Log out errors.
 		console.error(`[multi-semantic-release]:`, error);
-		process.exit(1);
+		exit(1);
 	}
 };
