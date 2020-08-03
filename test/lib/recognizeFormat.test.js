1dbfc4fde297f12bd38aed7d489ea5692aa456cb
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-08-03 19:21:05 +1000

    fix: preserve trailing whitespace in manifest (fixes #31)

---
+++ test/lib/recognizeFormat.test.js
@@ -0,0 +1,24 @@
+/** @typedef {import('@types/jest')} */
+const recognizeFormat = require("../../lib/recognizeFormat");
+
+// Tests.
+describe("recognizeFormat()", () => {
+	describe("Indentation", () => {
+		test("Normal indentation", () =>
+			expect(
+				recognizeFormat(`{
+	"a": "b",
+	"c": {
+		"d": "e"
+	}
+}`).indent
+			).toBe("\t"));
+		test("No indentation", () => expect(recognizeFormat('{"a": "b"}').indent).toBe(2));
+	});
+
+	describe("Trailing whitespace", () => {
+		test("No trailing whitespace", () => expect(recognizeFormat('{"a": "b"}').trailingWhitespace).toBe(""));
+		test("Newline", () => expect(recognizeFormat('{"a": "b"}\n').trailingWhitespace).toBe("\n"));
+		test("Multiple newlines", () => expect(recognizeFormat('{"a": "b"}\n\n').trailingWhitespace).toBe("\n\n"));
+	});
+});
