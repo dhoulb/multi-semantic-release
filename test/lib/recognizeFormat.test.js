ab723f10a55c6548aa911e9d0de84793bed64f7c
Anton Golub <golub.anton@gmail.com>
2020-09-23 23:32:01 +0400

    perf: replace custom format detector with detect-* utils

--- test/lib/recognizeFormat.test.js
+++ test/lib/recognizeFormat.test.js
@@ -13,12 +13,12 @@ describe("recognizeFormat()", () => {
 	}
 }`).indent
 			).toBe("\t"));
-		test("No indentation", () => expect(recognizeFormat('{"a": "b"}').indent).toBe(2));
+		test("No indentation", () => expect(recognizeFormat('{"a": "b"}').indent).toBe(""));
 	});
 
 	describe("Trailing whitespace", () => {
 		test("No trailing whitespace", () => expect(recognizeFormat('{"a": "b"}').trailingWhitespace).toBe(""));
 		test("Newline", () => expect(recognizeFormat('{"a": "b"}\n').trailingWhitespace).toBe("\n"));
-		test("Multiple newlines", () => expect(recognizeFormat('{"a": "b"}\n\n').trailingWhitespace).toBe("\n\n"));
+		test("Multiple newlines", () => expect(recognizeFormat('{"a": "b"}\n\n').trailingWhitespace).toBe("\n"));
 	});
 });
