cd53da2c86d799dd1c6f0555102044e832243483
Killian Hamayada <killian.hamayada@gmail.com>
2021-11-08 11:51:39 +0100

    chore: use proper path for binaries (#3)

--- dist/lib/glob.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const globby_1 = __importDefault(require("globby"));
-exports.default = (patterns, options) => globby_1.default.sync(patterns, options);
